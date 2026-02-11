9a492c5841b6389835abf3042d516e90b8260f6c
armkantawee <armkantawee@gmail.com>
2026-02-11 19:22:45 +0700

    Now I know

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 #print ("Hello World")
-print("Hello World")
-
-#print good bye
-print("good bye")
-
-#print new line
-print("I'm learning github.")
-
-
-#new function
-def add_two(num):
-  return num + 2
-
-print(add_two(2), add_two(5))
+print("Now I know what Git and Github is. Easy!")
